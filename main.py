6e7682cf181878b1c7c438ac8a9016ccd9af517b
Selim Sertbaş <selimsertbas@Selim-MacBook-Pro.local>
2021-11-01 15:31:30 +0300

    Commit V2

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-import sys
+import string
+import random
 
-print(sys.version)
-print("Heil Marry!")
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+numbers = "0123456789"
+symbols = "!^+%&?*-"
 
-test = "hello bro"
-print(test)
+def splitChar(word):
+    return [char for char in word]
 
-name = input("write your name:")
-print("your name is", name)
+upper = splitChar(upper)
+lower = splitChar(lower)
+numbers = splitChar(numbers)
+symbols = splitChar(symbols)
+
+
+def generatePass():
+    password = []
+    random.shuffle(upper)
+    random.shuffle(lower)
+    random.shuffle(numbers)
+    random.shuffle(symbols)
+
+    for i in range(4):
+        a = random.choice(upper)
+        password.append(a)
+    for i in range(2):
+        a = random.choice(lower)
+        password.append(a)
+    for i in range(2):
+        a = random.choice(numbers)
+        password.append(a)
+
+    
+
+    s = [str(i) for i in password]
+    random.shuffle(s)
+    password = ''.join(s)
+
+    print("Here is your randomly generated password: "+str(password))
+    
+
+print("There will be 4 letters lower, 2 lettes upper and 2 numbers in your password.")
+generatePass()
